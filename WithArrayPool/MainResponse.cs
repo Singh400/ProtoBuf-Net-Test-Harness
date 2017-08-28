8540386a2586b0c9b02f553a1371cdc3b09d6e7d
Singh400 <Singh400>
2017-08-28 18:38:33 +0100

    Adding System.Buffers.ArrayPool
    The test harness now has an implementation of protobuf-net that is backed by System.Buffers.ArrayPool for the Buffer Pool. This version shows the same reduction in LOH allocations (90%) as the WithCachedPool version.

---
+++ WithArrayPool/MainResponse.cs
@@ -0,0 +1,34 @@
+﻿using ProtoBuf;
+
+namespace WithArrayPool
+{
+    [ProtoContract]
+    public class MainResponse
+    {
+        [ProtoMember(1)]
+        public OuterResponse OuterResponse { get; set; }
+        [ProtoMember(2)]
+        public InnerResponse InnerResponse { get; set; }
+    }
+
+    [ProtoContract]
+    public class OuterResponse
+    {
+        [ProtoMember(1)]
+        public Result[] OuterResults { get; set; }
+    }
+
+    [ProtoContract]
+    public class InnerResponse
+    {
+        [ProtoMember(1)]
+        public Result[] InnerResults { get; set; }
+    }
+
+    [ProtoContract]
+    public class Result
+    {
+        [ProtoMember(1)]
+        public string TheString { get; set; }
+    }
+}
